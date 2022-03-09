8ede6cd8fbc92af4d01752d6040fda831bed9fa2
farid98 <farid98@gmail.com>
2022-03-09 10:53:07 +0500

    As in youtube

--- src/modal.js
+++ src/modal.js
@@ -11,18 +11,17 @@ const Modal = ({ setModalOn, setChoice }) => {
   
       return (
   
-          <div className="   bg-zinc-200 opacity-95 fixed inset-0 z-50   ">
+          <div className="   bg-zinc-200 opacity-80 fixed inset-0 z-50   ">
   
               <div className="flex h-screen justify-center items-center ">
   
-                  <div className="flex flex-col justify-center align-middle bg-white p-12 rounded-xl">
+                  <div className="flex-col justify-center  bg-white py-12 px-24 border-4 border-sky-500 rounded-xl ">
 
-                      <div className="flex  mb-10"  >Are you sure ?</div>
+                      <div className="flex  text-lg  text-zinc-600   mb-10" >Are you sure ?</div>
                       <div className="flex">
                           <button onClick={handleOKClick} className=" rounded px-4 py-2 text-white  bg-green-400 ">Yes</button>
                           <button onClick={handleCancelClick} className="rounded px-4 py-2 ml-4 text-white bg-blue-500 ">No</button>
                       </div>
-
 
                   </div>
               </div>
--- src/pasteboard.js
+++ src/pasteboard.js
@@ -1,9 +1,8 @@
 import Modal from "./modal";
-
 import { useState } from "react";
 
 
-const App = () => {
+function App() {
 
   const [modalOn, setModalOn] = useState(false);
   const [choice, setChoice] = useState(false)
@@ -13,33 +12,25 @@ const App = () => {
   }
 
   return (
-
     <div>
 
       <div className="flex justify-center">
         <div className="flex  cursor-pointer justify-center w-1/3  bg-blue-400 p-4  m-6 rounded-md text-white"
-
-          onClick={clicked}
-        >
+          onClick={clicked}>
           Bite Me
         </div>
       </div>
 
-    {/* conditionally display the result of the action if user confirms  */}
+
       {choice &&
       <div className="flex justify-center">
         <div className="flex  justify-center w-1/3 bg-red-400 m-4 p-6 text-lg text-white "> You have been bitten !!!</div>
         </div>
       }
 
-
       {modalOn && < Modal setModalOn={setModalOn} setChoice={setChoice} />}
 
-    </div>
-
-
-
-
+    </div >
   );
 }
 
